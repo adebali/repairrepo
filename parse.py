aacfd63915115eabe27a670c1fd6b9b154e2e9e1
Yashar Asgari <yasharasgari97@gmail.com>
2018-01-30 18:29:17 -0500

    changed data formatting
    added key 'organism'

--- parse.py
+++ parse.py
@@ -7,9 +7,11 @@ reader = csv.DictReader(txtfile, delimiter = '\t')
 
 connection = MongoClient('mongodb://yasgari:701415@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/sampleResultsDb?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
 db = connection.sampleResultsDb
-collection = db['human']
+collection = db['gene']
 
 for row in reader:
-    collection.insert(json.loads(json.dumps(row)))
+    data = dict(row)
+    data['organism'] = 'human'
+    collection.insert(json.loads(json.dumps(data)))
 
 connection.close()
