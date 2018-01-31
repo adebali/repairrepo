0d073b356592957b79c2724ccb506d1c69ec386f
Yashar Asgari <yasgari@ad.unc.edu>
2018-01-31 10:19:46 -0500

    Changed to push data to different DB

--- parse.py
+++ parse.py
@@ -8,8 +8,8 @@ txtfile = open('small.txt', 'r') #txt file with subset of sample data
 
 reader = csv.DictReader(txtfile, delimiter = '\t')
 
-connection = MongoClient('mongodb://USER:PASSWORD@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/sampleResultsDb?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
-db = connection.sampleResultsDb
+connection = MongoClient('mongodb://USER:PASSWORD@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/data?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
+db = connection.data
 collection = db['gene']
 
 for row in reader:
