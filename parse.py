7a1eebccbb97295fb721135484ea02d4969151d7
Yashar Asgari <yasgari@ad.unc.edu>
2018-01-31 09:50:39 -0500

    updated user and pass

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ txtfile = open('small.txt', 'r') #txt file with subset of sample data
 
 reader = csv.DictReader(txtfile, delimiter = '\t')
 
-connection = MongoClient('mongodb://yasgari:701415@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/sampleResultsDb?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
+connection = MongoClient('mongodb://USER:PASSWORD@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/sampleResultsDb?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
 db = connection.sampleResultsDb
 collection = db['gene']
 
