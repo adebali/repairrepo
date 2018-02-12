54f6f551e4ddfb076f13c3e4f4ccc0d4b9b683fb
Yashar Asgari <yasgari@ad.unc.edu>
2018-02-12 11:44:00 -0500

    updated

--- flushDB.py
+++ flushDB.py
@@ -1,4 +1,4 @@
-#Push data from file to pymongo db
+#Flush data from DB
 #Author: Yashar Asgari
 
 import pymongo
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 
 connection = MongoClient('mongodb://USER:PASSWORD@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/data?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
 db = connection.data
-collection = db['gene'].delete_many({}) #try this method
+collection = db['gene'].delete_many({})
 
 connection.close()
 
