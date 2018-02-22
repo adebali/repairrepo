1a6533ef5a53d8d104e6073044a225fc30475b75
Yashar Asgari <yasgari@ad.unc.edu>
2018-02-21 21:56:01 -0500

    adding argparse

--- update.py
+++ update.py
@@ -1,11 +1,18 @@
 #Push data from file to pymongo db
 #Author: Yashar Asgari
-import csv, json, pymongo
+import csv, json, pymongo, argparse
 from pymongo import MongoClient
 
-txtfile = open('testUpsert.txt', 'r') #txt file with subset of sample data
+
+parser = argparse.ArgumentParser(description = 'argparse for organism type')
+parser.add_argument('organism')
+args = parser.parse_args()
+print('here is the arg: ' + args.organism)
+
+#txtfile = open('testUpsert.txt', 'r') #txt file with subset of sample data
 
 reader = csv.DictReader(txtfile, delimiter = '\t')
+
 
 connection = MongoClient('mongodb://yasgari:701415@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/data?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
 db = connection.data
@@ -18,7 +25,7 @@ for row in reader:
     geneName = data['name']
 
 
-    cursor = collection.find({'$and':[{"organism":"mouse"}, {"name": geneName}]})
+    cursor = collection.find({'$and':[{"organism":args.organism}, {"name": geneName}]})
 
     if(cursor.count() == 0):
         
@@ -27,9 +34,9 @@ for row in reader:
             if(iterData[key] is None):
                 data.pop(key,None)
 
-        collection.insert(data)
+        #collection.insert(data)
     elif(cursor.count() > 1): #there should only be 1 document for each organism/gene pair
-        raise ValueError("Duplicate document found. More than one found for Gene " + geneName)
+        raise ValueError("Duplicate document found. More than one found for Gene " + geneName + " for " + args.organism)
 
     for oldDoc in cursor: 
             for key in data.keys():
@@ -40,7 +47,7 @@ for row in reader:
                     oldDoc.pop(key, None) #get rid of null fields
                 else:
                     #update using document's unique ID
-                    collection.update({"_id": uqId}, {"$set":{key:oldDoc[key]}})    
+                    #collection.update({"_id": uqId}, {"$set":{key:oldDoc[key]}})    
 
 
 connection.close()
