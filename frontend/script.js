929c8b00c951b059547145b241fc31a4d27231fa
Yashar Asgari <yasharasgari97@gmail.com>
2018-11-04 16:09:43 -0500

    revert

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,4 @@
 //Script for RepairRepo database project's search page- written by Yashar Asgari
-import Stitch from "mongodb-stitch-browser-sdk";
-import RemoteMongoClient from "mongodb-stitch-browser-sdk";
-import AnonymousCredential from "mongodb-stitch-browser-sdk";
 $(document).ready(function(){
     
       
@@ -20,20 +17,6 @@ $(document).ready(function(){
             //query db for sample sheet in order to get columns/data for each experiment for plotting purposes
             getSampleSheet();     
         
-            const client = Stitch.initializeDefaultAppClient('dataretrieval-vwdtg');
-            
-            const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('data');
-            
-            client.auth.loginWithCredential(new AnonymousCredential()).then(user => 
-              db.collection('gene').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
-            ).then(() => 
-              db.collection('gene').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
-            ).then(docs => {
-                console.log("Found docs", docs)
-                console.log("[MongoDB Stitch] Connected to Stitch")
-            }).catch(err => {
-                console.error(err)
-            });
         
             //prep server connection
             const clientPromise = stitch.StitchClientFactory.create('dataretrieval-vwdtg');
