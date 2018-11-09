b5c35017fe02c60aaadfe2937c573d13cd67e821
Yashar Asgari <yasharasgari97@gmail.com>
2018-11-09 18:26:41 -0500

    test again

--- frontend/script.js
+++ frontend/script.js
@@ -440,7 +440,7 @@ $(document).ready(function(){
                 //create table headers
                 for (var index in array[0]) {
                     //get rid of _id column
-                    if(!(index.localeCompare("_id") == 0) || !(index.localeCompare("number")==0)){
+                    if((index.localeCompare("_id") == 0) || (index.localeCompare("number")==0)){
                     str += '<th scope="col">' + index + '</th>';
                     }
                 }
@@ -453,7 +453,7 @@ $(document).ready(function(){
                     for (var index in array[i]) {
                         //get rid of _id column
                         console.log(index)
-                        if(!(index.localeCompare("_id") == 0) || !(index.localeCompare("number")==0)){
+                        if((index.localeCompare("_id") == 0) || (index.localeCompare("number")==0)){
                             str += '<td>' + array[i][index] + '</td>';
 
                         }
