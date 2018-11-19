19d327c06a45e0e776513a310bf1b719d38b04a9
Yashar Asgari <yasharasgari97@gmail.com>
2018-11-19 14:35:10 -0500

    move code block

--- frontend/script.js
+++ frontend/script.js
@@ -334,28 +334,7 @@ $(document).ready(function(){
             //     }
             // });
 
-            //stable highlight rows
-            var preEl ;
-            var orgBColor;
-            var orgTColor;
-            function HighLightTR(el, backColor,textColor){
-              if(typeof(preEl)!='undefined') {
-                 preEl.bgColor=orgBColor;
-                 try{ChangeTextColor(preEl,orgTColor);}catch(e){;}
-              }
-              orgBColor = el.bgColor;
-              orgTColor = el.style.color;
-              el.bgColor=backColor;
             
-              try{ChangeTextColor(el,textColor);}catch(e){;}
-              preEl = el;
-            }
-            
-            
-            function ChangeTextColor(a_obj,a_color){  ;
-               for (i=0;i<a_obj.cells.length;i++)
-                a_obj.cells(i).style.color=a_color;
-            }
         
             //CHANGE handlers for input fields
             $('#startChr').change(function(){
@@ -473,6 +452,30 @@ $(document).ready(function(){
                 $("#nameHover").empty();
             })
            
+            //stable highlight rows
+            var preEl ;
+            var orgBColor;
+            var orgTColor;
+            function HighLightTR(el, backColor,textColor){
+              if(typeof(preEl)!='undefined') {
+                 preEl.bgColor=orgBColor;
+                 try{ChangeTextColor(preEl,orgTColor);}catch(e){;}
+              }
+              orgBColor = el.bgColor;
+              orgTColor = el.style.color;
+              el.bgColor=backColor;
+            
+              try{ChangeTextColor(el,textColor);}catch(e){;}
+              preEl = el;
+            }
+            
+            
+            function ChangeTextColor(a_obj,a_color){  ;
+               for (i=0;i<a_obj.cells.length;i++)
+                a_obj.cells(i).style.color=a_color;
+            }
+
+
             /**
              * Given the results from a query, the method will build a table with embedded click 
              * event handlers for each row (to enable plots to be shown)
