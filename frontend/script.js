2ff4ce0a764ac5e548b379b11e929f350e56f0d5
Yashar Asgari <yasharasgari97@gmail.com>
2018-11-28 13:44:51 -0500

    remove brackets from arg1

--- frontend/script.js
+++ frontend/script.js
@@ -66,8 +66,8 @@ $(document).ready(function(){
             function queryChr(arg1, prev, first){
                 
                 //arg1 = arg1.length > 0 ? { $and: arg1 } : {};
-                
-                console.log("ar1" + JSON.stringify(arg1))
+                var arg1 = arg1.substring(1, arg1.length-1);
+                console.log("arg1" + JSON.stringify(arg1))
                 //show number on page out of total returned from query 
                 if(first){
                     db.collection('gene').find(arg1).execute().then(temp =>{
