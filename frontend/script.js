54435549fe3b4bc10c77e408a343ef61f5f7a0a6
Yashar Asgari <yasgari@ad.unc.edu>
2018-12-17 15:29:27 -0500

    add igv implementaiton to script.js

--- frontend/script.js
+++ frontend/script.js
@@ -636,6 +636,29 @@ $(document).ready(function(){
                     return buildChrDropdown2();
                 });
             }
+
+            //CODE FOR IGV.JS GENOME BROSWER 
+
+            var igv =  document.getElementById("igvContainer");
+            var options = {
+                genome: "hg19",
+                locus: "chr8:128,747,267-128,754,546",
+                tracks: [
+                    {
+                        type: 'alignment',
+                        format: 'bam',
+                        url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                        name: 'HG02450'
+                    }
+                ]
+            };
+            igv.createBrowser(igvDiv, options)
+            .then(function (browser) {
+                console.log("Created IGV browser");
+            })
+
+
+
         
         //CODE FOR EXPERIMENT PLOTS BELOW
         
