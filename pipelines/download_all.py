73752ef13183b3df072bc5118e39ad0ac365be4d
Adebali <adebali@users.noreply.github.com>
2019-01-07 15:49:15 +0300

    data location option added

--- pipelines/download_all.py
+++ pipelines/download_all.py
@@ -4,14 +4,17 @@ import json
 
 parser = argparse.ArgumentParser(description='download all files through fastq')
 parser.add_argument('-i', type=str, required= True, help='SRA ID(s)')
+parser.add_argument('-d', type=str, default="/data", help='data location')
 args = parser.parse_args()
+dataLocation = args.d
 
 inputFile = args.i
 code = ''
 
 j = json.load(open(inputFile))
 for sample in j:
+    if not os.path.isfile
     IDs = sample["SRA"]["runs"]
-    code = "python3 fastq-dump-all.py -i " + " ".join(IDs) + " -o " + sample["title"] + ".fastq"
+    code = "python3 fastq-dump-all.py -i " + " ".join(IDs) + " -o " + os.path.join(dataLocation, sample["title"] + ".fastq")
     print(code)
     # os.system(code)
