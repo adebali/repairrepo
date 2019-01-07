3a55306844d765ca8164b9b914fc6db99c49acb7
Adebali <adebali@users.noreply.github.com>
2019-01-07 15:50:16 +0300

    outputFile added

--- pipelines/download_all.py
+++ pipelines/download_all.py
@@ -7,14 +7,15 @@ parser.add_argument('-i', type=str, required= True, help='SRA ID(s)')
 parser.add_argument('-d', type=str, default="/data", help='data location')
 args = parser.parse_args()
 dataLocation = args.d
+outputFile = os.path.join(dataLocation, sample["title"] + ".fastq")
 
 inputFile = args.i
 code = ''
 
 j = json.load(open(inputFile))
 for sample in j:
-    if not os.path.isfile
-    IDs = sample["SRA"]["runs"]
-    code = "python3 fastq-dump-all.py -i " + " ".join(IDs) + " -o " + os.path.join(dataLocation, sample["title"] + ".fastq")
-    print(code)
-    # os.system(code)
+    if not os.path.isfile(outputFile):
+        IDs = sample["SRA"]["runs"]
+        code = "python3 fastq-dump-all.py -i " + " ".join(IDs) + " -o " + outputFile
+        print(code)
+        # os.system(code)
