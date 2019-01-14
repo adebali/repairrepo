4f2f7ce72718375118391c15e060213595bf1be3
Adebali <adebali@users.noreply.github.com>
2019-01-14 12:40:39 +0300

    download and merge fastq

--- pipelines/merge_fastq.py
+++ pipelines/merge_fastq.py
@@ -12,13 +12,22 @@ dataLocation = args.d
 inputFile = args.i
 code = ''
 
+
+def run(code):
+    print(code)
+    # os.system(code)
+
 j = json.load(open(inputFile))
 for sample in j:
     outputFile = os.path.join(dataLocation, sample["title"] + ".fastq")
     if not os.path.isfile(outputFile):
         IDs = sample["SRA"]["runs"]
-        for ID in IDs:
-        #code = "python fastq-dump-multi.py -i " + " ".join(IDs) + " -o " + outputFile
-            code = "parallel-fastq-dump --threads 8 --sra-id " + str(ID) + " --outdir " + args.d
-            print(code)
-            os.system(code)
+        if len(IDs) == 1:
+            code = "mv " + os.path.join(dataLocation, IDs[0]) + ".fastq" + " " + outputFile
+            run(code)
+        else:
+            code = "rm -f " + outputFile
+            run(code)
+            for ID in IDs:
+                code = "cat " + os.path.join(dataLocation, ID) + ".fastq" + " >>" + outputFile
+                run(code)
