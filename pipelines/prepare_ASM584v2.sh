d2c7977884acb308f40804c5a408c09eacc0cac0
Adebali <adebali@users.noreply.github.com>
2019-01-16 17:24:35 +0300

    variables added to handle all at once

--- pipelines/prepare_ASM584v2.sh
+++ pipelines/prepare_ASM584v2.sh
@@ -2,15 +2,18 @@
 ORGANISM="Escherichia_coli_str_k_12_substr_mg1655"
 GENOME="ASM584v2"
 ENSEMBL_VERSION="42"
-ENSEMBL_BASE="ftp://ftp.ensembl.org/pub"
+ENSEMBL_BASE_BASE="ftp.ensemblgenomes.org"
+ENSEMBL_BASE="ftp://${ENSEMBL_BASE_BASE}/pub"
+FASTA_MID="bacteria/fasta/bacteria_0_collection"
+GFF3_MID="bacteria/gff3/bacteria_0_collection"
 GENOMES_DIRECTORY=../data
 WORKING_DIRECTORY=../data
 
 cd $WORKING_DIRECTORY
 
-wget -r --no-parent -A ${ORGANISM}.${GENOME}.dna.chromosome.*.fa.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/bacteria/fasta/bacteria_0_collection/${ORGANISM,,}/dna/
+wget -r --no-parent -A ${ORGANISM}.${GENOME}.dna.chromosome.*.fa.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/${FASTA_MID}/${ORGANISM,,}/dna/
 
-cd ftp.ensembl.org/pub/release-${ENSEMBL_VERSION}/fasta/${ORGANISM,,}/dna
+cd ${ENSEMBL_BASE_BASE}/pub/release-${ENSEMBL_VERSION}/fasta/${ORGANISM,,}/dna
 mkdir -p ${GENOMES_DIRECTORY}/${GENOME}/Bowtie2
 
 bowtie2-build --threads 4 -f \
@@ -18,4 +21,4 @@ ${ORGANISM}.${GENOME}.dna.chromosome.Chromosome.fa.gz \
 ${GENOMES_DIRECTORY}/${GENOME}/Bowtie2/genome
 
 zcat *gz >${GENOMES_DIRECTORY}/${GENOME}/genome.fa && cd ${GENOMES_DIRECTORY}/${GENOME} && samtools faidx genome.fa
-wget -O ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/bacteria/gff3/bacteria_0_collection/${ORGANISM,,}/${ORGANISM}.${GENOME}.${ENSEMBL_VERSION}.gff3.gz && gunzip ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz && gff2bed.py -i ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3 -r gene -o ${GENOMES_DIRECTORY}/${GENOME}/genes.bed && rm ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3
+wget -O ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/${GFF3_MID}/${ORGANISM,,}/${ORGANISM}.${GENOME}.${ENSEMBL_VERSION}.gff3.gz && gunzip ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz && gff2bed.py -i ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3 -r gene -o ${GENOMES_DIRECTORY}/${GENOME}/genes.bed && rm ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3
--- pipelines/prepare_GRCh38.sh
+++ pipelines/prepare_GRCh38.sh
@@ -2,15 +2,18 @@
 ORGANISM="Homo_sapiens"
 GENOME="GRCh38"
 ENSEMBL_VERSION="92"
-ENSEMBL_BASE="ftp://ftp.ensembl.org/pub"
+ENSEMBL_BASE_BASE="ftp.ensembl.org"
+ENSEMBL_BASE="ftp://${ENSEMBL_BASE_BASE}/pub"
+FASTA_MID="fasta"
+GFF3_MID="gff3"
 GENOMES_DIRECTORY=../data
 WORKING_DIRECTORY=../data
 
 cd $WORKING_DIRECTORY
 
-wget -r --no-parent -A ${ORGANISM}.${GENOME}.dna.chromosome.*.fa.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/fasta/${ORGANISM,,}/dna/
+wget -r --no-parent -A ${ORGANISM}.${GENOME}.dna.chromosome.*.fa.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/${FASTA_MID}/${ORGANISM,,}/dna/
 
-cd ftp.ensembl.org/pub/release-${ENSEMBL_VERSION}/fasta/${ORGANISM,,}/dna
+cd ${ENSEMBL_BASE_BASE}/pub/release-${ENSEMBL_VERSION}/fasta/${ORGANISM,,}/dna
 mkdir -p ${GENOMES_DIRECTORY}/${GENOME}/Bowtie2
 
 bowtie2-build --threads 4 -f \
@@ -42,4 +45,4 @@ ${ORGANISM}.${GENOME}.dna.chromosome.MT.fa.gz \
 ${GENOMES_DIRECTORY}/${GENOME}/Bowtie2/genome
 
 zcat *gz >${GENOMES_DIRECTORY}/${GENOME}/genome.fa && cd ${GENOMES_DIRECTORY}/${GENOME} && samtools faidx genome.fa
-wget -O ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/gff3/${ORGANISM,,}/${ORGANISM}.${GENOME}.${ENSEMBL_VERSION}.gff3.gz && gunzip ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz && gff2bed.py -i ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3 -r gene -o ${GENOMES_DIRECTORY}/${GENOME}/genes.bed && rm ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3
+wget -O ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/${GFF3_MID}/${ORGANISM,,}/${ORGANISM}.${GENOME}.${ENSEMBL_VERSION}.gff3.gz && gunzip ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz && gff2bed.py -i ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3 -r gene -o ${GENOMES_DIRECTORY}/${GENOME}/genes.bed && rm ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3
