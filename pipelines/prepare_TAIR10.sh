c86698b1634de955b96a49abea9784b1921d0986
Adebali <adebali@users.noreply.github.com>
2019-01-16 16:41:33 +0300

    TAIR10 prepare

---
+++ pipelines/prepare_TAIR10.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+ORGANISM="Arabidopsis_thaliana"
+GENOME="TAIR10"
+ENSEMBL_VERSION="42"
+ENSEMBL_BASE="ftp://ftp.ensemblgenomes.org/pub"
+GENOMES_DIRECTORY=../data
+WORKING_DIRECTORY=../data
+
+cd $WORKING_DIRECTORY
+
+wget -r --no-parent -A ${ORGANISM}.${GENOME}.dna.chromosome.*.fa.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/plants/fasta/${ORGANISM,,}/dna/
+
+cd ftp.ensembl.org/pub/release-${ENSEMBL_VERSION}/fasta/${ORGANISM,,}/dna
+mkdir -p ${GENOMES_DIRECTORY}/${GENOME}/Bowtie2
+
+bowtie2-build --threads 4 -f \
+${ORGANISM}.${GENOME}.dna.chromosome.1.fa.gz,\
+${ORGANISM}.${GENOME}.dna.chromosome.2.fa.gz,\
+${ORGANISM}.${GENOME}.dna.chromosome.3.fa.gz,\
+${ORGANISM}.${GENOME}.dna.chromosome.4.fa.gz,\
+${ORGANISM}.${GENOME}.dna.chromosome.5.fa.gz,\
+${ORGANISM}.${GENOME}.dna.chromosome.Mt.fa.gz, \
+${ORGANISM}.${GENOME}.dna.chromosome.Pt.fa.gz \
+${GENOMES_DIRECTORY}/${GENOME}/Bowtie2/genome
+
+zcat *gz >${GENOMES_DIRECTORY}/${GENOME}/genome.fa && cd ${GENOMES_DIRECTORY}/${GENOME} && samtools faidx genome.fa
+wget -O ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz ${ENSEMBL_BASE}/release-${ENSEMBL_VERSION}/plants/gff3/${ORGANISM,,}/${ORGANISM}.${GENOME}.${ENSEMBL_VERSION}.gff3.gz && gunzip ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3.gz && gff2bed.py -i ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3 -r gene -o ${GENOMES_DIRECTORY}/${GENOME}/genes.bed && rm ${GENOMES_DIRECTORY}/${GENOME}/genes.gff3
