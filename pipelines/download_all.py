384ba9a848a7b6584c208589d077c77fe7149b2c
Adebali <adebali@users.noreply.github.com>
2019-01-16 11:48:35 +0300

    single script for download and name change

--- pipelines/download_all.py
+++ pipelines/download_all.py
@@ -12,13 +12,22 @@ dataLocation = args.d
 inputFile = args.i
 code = ''
 
+def run(code):
+    print(code)
+    os.system(code)
+
 j = json.load(open(inputFile))
 for sample in j:
     outputFile = os.path.join(dataLocation, sample["title"] + ".fastq")
     if not os.path.isfile(outputFile):
         IDs = sample["SRA"]["runs"]
         for ID in IDs:
-        #code = "python fastq-dump-multi.py -i " + " ".join(IDs) + " -o " + outputFile
-            code = "parallel-fastq-dump --threads 8 --sra-id " + str(ID) + " --outdir " + args.d
-            print(code)
-            os.system(code)
+            run("parallel-fastq-dump --threads 8 --sra-id " + str(ID) + " --outdir " + args.d)
+
+        if len(IDs) == 1:
+            run("mv " + os.path.join(dataLocation, IDs[0]) + ".fastq" + " " + outputFile)
+        else:
+            run("rm -f " + outputFile)
+            for ID in IDs:
+                run("cat " + os.path.join(dataLocation, ID) + ".fastq" + " >>" + outputFile)
+                run("rm " + os.path.join(dataLocation, ID) + ".fastq")
