e222b34ed0a0da156fe5fb03647b6d73ed3e1c7d
Yashar Asgari <yasgari@ad.unc.edu>
2019-02-07 15:09:29 -0500

    change

--- frontend/browserScript.js
+++ frontend/browserScript.js
@@ -5,37 +5,37 @@ $(document).ready(function(){
     $('#footer').load('footer.html');
     $('#sidebar').load('sidebar.html');
 
-    const organism = getCurrentOrganism();
+    //const organism = getCurrentOrganism();
     console.log('current organism: ' + organism)
 
     //CODE FOR IGV.JS GENOME BROSWER 
 
-    // function createBrowser(){
-    //     var igvDiv =  document.getElementById("igvContainer");
-    //     var options = {
-    //         genome: "hg19",
-    //         locus: "chr8:128,747,267-128,754,546",
-    //         tracks: [
-    //             {
-    //                 type: 'alignment',
-    //                 format: 'bam',
-    //                 url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
-    //                 name: 'HG02450'
-    //             },
-    //             //second track, use example from igv to toggle certain tracks on and off
-    //             {
-    //                 type: 'alignment',
-    //                 format: 'bam',
-    //                 url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
-    //                 name: 'HG02450'
-    //             }
-    //         ]
-    //     };
-    //     igv.createBrowser(igvDiv, options)
-    //     .then(function (browser) {
-    //         console.log("Created IGV browser");
-    //     })
-    // }
+    function createBrowser(){
+        var igvDiv =  document.getElementById("igvContainer");
+        var options = {
+            genome: "hg19",
+            locus: "chr8:128,747,267-128,754,546",
+            tracks: [
+                {
+                    type: 'alignment',
+                    format: 'bam',
+                    url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                    name: 'HG02450'
+                },
+                //second track, use example from igv to toggle certain tracks on and off
+                {
+                    type: 'alignment',
+                    format: 'bam',
+                    url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                    name: 'HG02450'
+                }
+            ]
+        };
+        igv.createBrowser(igvDiv, options)
+        .then(function (browser) {
+            console.log("Created IGV browser");
+        })
+    }
 
     //TODO
 
