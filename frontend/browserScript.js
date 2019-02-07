552c3d9a757863641531c8c4a360094116fbe2a5
Yashar Asgari <yasgari@ad.unc.edu>
2019-02-07 15:08:03 -0500

    remove func call

--- frontend/browserScript.js
+++ frontend/browserScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-    $('#header').load('header.html';
+    $('#header').load('header.html');
     $('#footer').load('footer.html');
     $('#sidebar').load('sidebar.html');
 
