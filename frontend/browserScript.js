1633c570181938ca1c20d34ed915f62b6012f4c7
Yashar Asgari <yasgari@ad.unc.edu>
2019-02-17 16:48:33 -0500

    revert

--- frontend/browserScript.js
+++ frontend/browserScript.js
@@ -8,7 +8,32 @@ $(document).ready(function(){
         const organism = getCurrentOrganism();
         console.log('current organism: ' + organism)
     
-
+        //CODE FOR IGV.JS GENOME BROSWER 
+    
+        var igvDiv =  document.getElementById("igvContainer");
+        var options = {
+            genome: "hg19",
+            locus: "chr8:128,747,267-128,754,546",
+            tracks: [
+                {
+                    type: 'alignment',
+                    format: 'bam',
+                    url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                    name: 'HG02450'
+                },
+                //second track, use example from igv to toggle certain tracks on and off
+                {
+                    type: 'alignment',
+                    format: 'bam',
+                    url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                    name: 'HG02450'
+                }
+            ]
+        };
+        igv.createBrowser(igvDiv, options)
+        .then(function (browser) {
+            console.log("Created IGV browser");
+        })
     
         $(document).on("change", "textarea", function(){
             //array of tracks to be selected or removed
